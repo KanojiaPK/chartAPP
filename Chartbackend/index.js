3ffa3c393894f9ac59c429539d58c3722adf512e
Prince Kanojia <princekanojia7000@gmail.com>
2024-11-18 23:17:55 +0530

    cors updated

--- Chartbackend/index.js
+++ Chartbackend/index.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
   if (process.env.NODE_ENV === "production") {
     res.setHeader(
       "Access-Control-Allow-Origin",
-      "https://savysupport-supportticketmangerfrontend.onrender.com"
+      "https://chartapp-frontend.onrender.com"
     );
   } else {
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
